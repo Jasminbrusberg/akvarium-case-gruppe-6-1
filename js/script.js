7bd218ab228aea7f34ac2d300a4639c3fa588e4b
Jasminbrusberg <J-brusberg@hotmail.com>
2026-03-17 09:36:42 +0100

    1.commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+"use strict";
+const crab = document.getElementById("krabbe");
+const crabTooltip = document.getElementById("krabbeTooltip");
+
+krabbe.addEventListener("click", () => {
+  krabbeTooltip.classList.toggle("is-visible");
+});
+
+
